5dbb98986c52ac0b524d74b875b6d0ff2ff72bac
Anastasia <a.baranovskaya@softteco.com>
2025-01-13 15:30:43 +0300

    fix: soft delete and task migration

--- migrations/1736585787556-Task.ts
+++ migrations/1736585787556-Task.ts
@@ -1,106 +1,106 @@
 import { MigrationInterface, QueryRunner, Table, TableForeignKey } from "typeorm";
 
-export class  Task1736585787556 implements MigrationInterface {
-    name = ' Task1736585787556'
+export class Task1736585787556 implements MigrationInterface {
+    name = 'Task1736585787556';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
-           new Table({
-             name: 'task',
-             columns: [
-               {
-                 name: 'id',
-                 type: 'int',
-                 isPrimary: true,
-                 isGenerated: true,
-               },
-               {
-                 name: 'createdAt',
-                 type: 'timestamp',
-                 isNullable: false,
-                 default: 'now()',
-               },
-               {
-                 name: 'updatedAt',
-                 type: 'timestamp',
-                 isNullable: true,
-               },
-               {
-                 name: 'deletedAt',
-                 type: 'timestamp',
-                 isNullable: true,
-               },
-               {
-                 name: 'name',
-                 type: 'varchar',
-                 isNullable: false,
-               },
-               {
-                 name: 'creatorId',
-                 type: 'int',
-                 isNullable: false,
-               },
-               {
-                 name: 'priority',
-                 type: 'enum',
-                 enum: ['Low', 'Medium', 'High'],
-                 enumName: 'PriorityEnum',
-                 isNullable: false,
-                 default: 'Medium'
-               },
-               {
-                 name: 'boardId',
-                 type: 'int',
-                 isNullable: false,
-               },
-               {
-                 name: 'status',
-                 type: 'enum',
-                 enum: ['Backlog', 'New', 'ToDo', 'In progress', 'Done'],
-                 enumName: 'statusEnum',
-                 isNullable: false,
-                 default: 'Backlog'
-               },
-               {
-                 name: 'title',
-                 type: 'varchar',
-                 isNullable: true,
-               },
-               {
-                 name: 'assigneeId',
-                 type: 'int',
-                 isNullable: true,
-               },
-               {
-                 name: 'storyPoints',
-                 type: 'int',
-                 isNullable: true,
-               },
-               {
-                 name: 'rating',
-                 type: 'bigInt',
-                 isNullable: true,
-               },
-               {
-                 name: 'type',
-                 type: 'enum',
-                 enum: ['Epic', 'Story', 'Task'],
-                 enumName: 'typeEnum',
-                 isNullable: true,
-               },
-             ]
-           })
-         )  
-       
+            new Table({
+                name: 'task',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                    },
+                    {
+                        name: 'createdAt',
+                        type: 'timestamp',
+                        isNullable: false,
+                        default: 'now()',
+                    },
+                    {
+                        name: 'updatedAt',
+                        type: 'timestamp',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'deletedAt',
+                        type: 'timestamp',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'creatorId',
+                        type: 'int',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'priority',
+                        type: 'enum',
+                        enum: ['Low', 'Medium', 'High'],
+                        enumName: 'PriorityEnum',
+                        isNullable: false,
+                        default: "'Medium'",
+                    },
+                    {
+                        name: 'boardId',
+                        type: 'int',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'status',
+                        type: 'enum',
+                        enum: ['Backlog', 'New', 'ToDo', 'In progress', 'Done'],
+                        enumName: 'statusEnum',
+                        isNullable: false,
+                        default: "'Backlog'",
+                    },
+                    {
+                        name: 'title',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'assigneeId',
+                        type: 'int',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'storyPoints',
+                        type: 'int',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'rating',
+                        type: 'bigint',
+                        isNullable: true,
+                    },
+                    {
+                        name: 'type',
+                        type: 'enum',
+                        enum: ['Epic', 'Story', 'Task'],
+                        enumName: 'typeEnum',
+                        isNullable: true,
+                    },
+                ],
+            })
+        );
+
         await queryRunner.createForeignKey("task", new TableForeignKey({
-          columnNames: ["boardId"],
-          referencedColumnNames: ["id"],
-          referencedTableName: "board",
-          onDelete: "CASCADE"
+            columnNames: ["boardId"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "board",
+            onDelete: "CASCADE",
         }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('task',  true, true, true);
+        await queryRunner.dropTable('task', true, true, true);
     }
 }
